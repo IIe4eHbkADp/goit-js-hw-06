d9dc77774114ff019b1c1f23c9e432f28182fcde
Torianyk Alex <torianyk.alex@gmail.com>
2022-08-07 13:26:17 +0300

    done

--- js/task-09.js
+++ js/task-09.js
@@ -7,11 +7,6 @@ const widget = document.querySelector('.widget');
 const color = document.querySelector('.color');
 const btn = document.querySelector('.change-color');
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
 function changeColor(event) {
   body.style.backgroundColor = getRandomHexColor();
   color.textContent = body.style.backgroundColor;
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,42 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const control = document.querySelector('#controls input');
+const createBtn = document.querySelector('button[data-create');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const parentBox = document.querySelector('#boxes');
+
+function boxesChanges(event) {
+  control.setAttribute('count', Number(event.currentTarget.value));
+  console.log(Number(event.currentTarget.value));
+}
+control.addEventListener('input', boxesChanges);
+
+let baseBoxSize = 30;
+
+function createBoxes(amount) {
+  let countBoxes = Number(control.getAttribute('count'));
+
+  for (let i = 0; i < countBoxes; i += 1) {
+    baseBoxSize += 10;
+
+    const newBoxes = document.createElement('div');
+    newBoxes.style.background = getRandomHexColor();
+    newBoxes.style.height = baseBoxSize + 'px';
+    newBoxes.style.width = baseBoxSize + 'px';
+    newBoxes.style.margin = '10px';
+    newBoxes.classList.add('new-boxes');
+    parentBox.append(newBoxes);
+  }
+}
+createBtn.addEventListener('click', createBoxes);
+
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function destroyBoxes(event) {
+  while (parentBox.firstElementChild) {
+    parentBox.removeChild(parentBox.lastElementChild);
+  }
+  control.value = '';
+}
