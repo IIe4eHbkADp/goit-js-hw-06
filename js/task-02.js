a8ac1c13249a34f25ad149f3b12b84386cc966bb
Torianyk Alex <torianyk.alex@gmail.com>
2022-08-07 12:59:12 +0300

    done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients');
+
+const ingredientsList = ingredients.map(ingredient => {
+  const addElement = document.createElement('li');
+
+  addElement.textContent = ingredient;
+  addElement.classList.add('item');
+  return addElement;
+});
+console.log(ingredientsList);
+list.append(...ingredientsList);
