f5564eeb9223ef9ca0b9e011969bd35c7027f23e
Torianyk Alex <torianyk.alex@gmail.com>
2022-08-07 20:19:26 +0300

    filal

--- js/task-01.js
+++ js/task-01.js
@@ -2,8 +2,7 @@ const categories = document.querySelectorAll('li.item');
 const numberOfCategories = categories.length;
 console.log('Number of categories:', numberOfCategories);
 
-const element = document.querySelectorAll('#categories>li');
-element.forEach(elem => {
+categories.forEach(elem => {
   console.log(
     `Category: ${elem.firstElementChild.textContent}, 
     Elements: ${elem.lastElementChild.children.length}`,
